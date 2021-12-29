31cb1b3325182869a3be20ec465e878e432646c0
Kody Fintak <kody.fintak@gmail.com>
2021-12-29 13:10:27 -0800

    raco new package

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "sample")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/sample.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(kodyfintak))
+(define license '(Apache-2.0 OR MIT))
---
+++ scribblings/sample.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[sample
+                    racket/base]]
+
+@title{sample}
+@author{kodyfintak}
+
+@defmodule[sample]
+
+Package Description Here
