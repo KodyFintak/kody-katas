59c619e7ca88620c4b62f01164b384358e62c759
Kody Fintak <kody.fintak@gmail.com>
2023-04-03 17:54:00 -0400

    implement rule 4 of dead cells becoming live cells

--- kody_katas/tests/test___init__.py
+++ kody_katas/tests/test___init__.py
@@ -20,23 +20,57 @@ from unittest import TestCase
 #    4. Any dead cell with exactly three live neighbours becomes a live cell.
 
 def next_grid(input):
-    
+
     result = []
 
-    for k in range(len(input)):
-        row = input[k]
+    for row_index in range(len(input)):
+        row = input[row_index]
         new_row = []
 
         for i in range(len(row)):
+            neighbors = []
+
+            get_neighbors_below(i, input, row_index, neighbors, row)
+            if i + 1 < len(row):
+                neighbors.append(row[i + 1])
+
+            number_of_live_neighbors = sum(neighbors)
+
             cell = row[i]
-            new_row.append(0)
+
+            if not cell and number_of_live_neighbors == 3:
+                new_row.append(1)
+            else:
+                new_row.append(0)
+
 
         result.append(new_row)
-    
+
     return result
 
 
+def get_neighbors_below(i, input, row_index, neighbors, row):
+    if row_index + 1 < len(row):
+        if i - 1 >= 0:
+            neighbors.append(input[row_index + 1][i - 1])
+
+        neighbors.append(input[row_index + 1][i])
+
+        if i + 1 < len(row):
+            neighbors.append(input[row_index + 1][i + 1])
+
+
 class Test(TestCase):
+
+    def test_generate_life(self):
+        value = [
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 0, 0]
+        ]
+
+        new_grid = next_grid(value)
+        self.assertEqual(new_grid[0][0], 1)
 
     def test_first(self):
         value = [
@@ -63,7 +97,7 @@ class Test(TestCase):
         ]
 
         new_grid = next_grid(value)
-        
+
         self.assertEqual(new_grid, expected)
 
     def test_make_matrix_dead(self):
@@ -83,6 +117,6 @@ class Test(TestCase):
         ]
 
         new_grid = next_grid(value)
-        
+
         self.assertEqual(new_grid, expected)
 
