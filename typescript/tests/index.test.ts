f22c58f3b036b2973508c83edb3489f12444973d
Sam Taggart <sam@sasworkshops.com>
2025-08-29 15:34:00 -0600

    Added sam

--- typescript/tests/index.test.ts
+++ typescript/tests/index.test.ts
@@ -1,6 +1,5 @@
 import {GildedRose} from '../src';
-
-// kody, ellis, rob
+// kody, ellis, rob. sam
 describe('test', () => {
     it('should add 1 + 2', () => {
         const rose = new GildedRose();
