b2c582cbadbcfa9926444806be0a7be8072701f6
Matthew Plavcan <mwplavcan@gmail.com>
2025-08-29 15:34:12 -0700

    Added normal item past sellIn date

--- typescript/tests/index.test.ts
+++ typescript/tests/index.test.ts
@@ -13,7 +13,7 @@ describe('Gilded Rose Approval', () => {
     it('should "Sulfuras", being a legendary item, never has to be sold or decreases in Quality', () => {
         const gildedRose = new GildedRose([
           new Item('Sulfuras, Hand of Ragnaros', 0, 5),
-          new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+          new Item("Sulfuras, Hand of Ragnaros", 0, 80),
           new Item("Sulfuras, Hand of Ragnaros", -1, 80)
         ]);
         const items = gildedRose.updateQuality();
@@ -22,5 +22,14 @@ describe('Gilded Rose Approval', () => {
         expect(items).toMatchSnapshot();
     });
 
+  it('should Once the sell by date has passed, `Quality` degrades twice as fast', () => {
+    const gildedRose = new GildedRose([
+      new Item("+5 Dexterity Vest", 0, 20)
+    ]);
+    const items = gildedRose.updateQuality();
+
+
+    expect(items).toMatchSnapshot();
+  });
 
 });
