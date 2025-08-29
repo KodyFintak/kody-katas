5d088f97829052d33681adbb260c344ae105fdb5
Rob Ratcliffe <rlr@tcliffe.dev>
2025-08-29 17:13:46 -0500

    add sulfaras test

--- typescript/tests/index.test.ts
+++ typescript/tests/index.test.ts
@@ -2,8 +2,16 @@ import { GildedRose, Item } from '../src';
 
 // kody, ellis, rob, sam, matt
 describe('Gilded Rose Approval', () => {
+    // sellIn, quality
     it('should foo', () => {
-        const gildedRose = new GildedRose([new Item('foo2', 0, 0)]);
+        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+        const items = gildedRose.updateQuality();
+
+        expect(items).toMatchSnapshot();
+    });
+
+    it('should sulfaras', () => {
+        const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 5)]);
         const items = gildedRose.updateQuality();
 
         expect(items).toMatchSnapshot();
