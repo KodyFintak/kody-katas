d4ef450710be5430e9380344228c5913b97b4b05
Kody Fintak <kody.fintak@gmail.com>
2026-02-28 11:52:35 -0500

    clean: extract sendHttpRequest

--- typescript/src/client/index.ts
+++ typescript/src/client/index.ts
@@ -1,15 +1,18 @@
 import * as http from 'node:http';
+import { IncomingMessage } from 'node:http';
 
-const request = http.request(
-  {
-    hostname: 'localhost',
-    port: 3000,
-    path: '/',
-    method: 'GET'
-  },
-  response => {
-    console.log(response.statusCode);
-  }
-);
+function sendHttpRequest(options: any): Promise<IncomingMessage> {
+  return new Promise((resolve, reject) => {
+    const request = http.request(options, response => {
+      resolve(response);
+    });
+    request.end();
+  });
+}
 
-request.end();
+sendHttpRequest({
+  hostname: 'localhost',
+  port: 3000,
+  path: '/',
+  method: 'GET'
+}).then(response => console.log(response.statusCode));
