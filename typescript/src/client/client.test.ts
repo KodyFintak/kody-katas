e0c1d6a0ca97597992d997c9d690c43097e7d8f3
Kody Fintak <kody.fintak@gmail.com>
2026-02-28 12:00:20 -0500

    feat: httpclient uses stub node http

--- typescript/src/client/client.test.ts
+++ typescript/src/client/client.test.ts
@@ -1,12 +1,22 @@
 class HttpClient {
+  constructor(private stubNodeHttp: StubNodeHttp) {}
+
   async sendRequest() {
-    return { status: 200 };
+    return this.stubNodeHttp.send();
+  }
+}
+
+class StubNodeHttp {
+  constructor(private options: { status: number }) {}
+  async send() {
+    return this.options;
   }
 }
 
 describe('client test', () => {
   it('sends a http request', async () => {
-    const client = new HttpClient();
+    const stubNodeHttp = new StubNodeHttp({ status: 200 });
+    const client = new HttpClient(stubNodeHttp);
     const response = await client.sendRequest();
     expect(response).toEqual({ status: 200 });
   });
