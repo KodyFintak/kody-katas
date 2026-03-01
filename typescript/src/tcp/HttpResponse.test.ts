ba46f4739ca9ce372af6c4c1eb74d006f1b0fa49
Kody Fintak <kody.fintak@gmail.com>
2026-03-01 12:12:25 -0500

    wip: start to add header

--- typescript/src/tcp/HttpResponse.test.ts
+++ typescript/src/tcp/HttpResponse.test.ts
@@ -2,10 +2,10 @@ class HttpResponse {
   constructor(private message: { httpVersion: number; status: number; reasonPhrase?: string }) {}
 
   toString() {
-    return this.statusLine();
+    return `${[this.statusLine(), 'date: Tue, 29 Oct 2024 16:56:32 GMT'].join('\r\n')}\r\n\r\n`;
   }
 
-  private statusLine() {
+  statusLine() {
     const statusLine = `HTTP/${this.message.httpVersion} ${this.message.status}`;
     if (this.message.reasonPhrase) return `${statusLine} ${this.message.reasonPhrase}`;
     return statusLine;
@@ -14,22 +14,22 @@ class HttpResponse {
 
 describe('HttpResponse', () => {
   it('creates HttpResponse', () => {
-    const response = new HttpResponse({ httpVersion: 1.1, status: 200 });
-    expect(response.toString()).toEqual('HTTP/1.1 200');
+    const response = new HttpResponse({ httpVersion: 1.1, status: 200, headers: { date: 'Tue, 29 Oct 2024 16:56:32 GMT' } });
+    expect(response.toString()).toEqual('HTTP/1.1 200\r\ndate: Tue, 29 Oct 2024 16:56:32 GMT\r\n\r\n');
   });
 
   it('creates HttpResponse with 1.2 version', () => {
     const response = new HttpResponse({ httpVersion: 1.2, status: 200 });
-    expect(response.toString()).toEqual('HTTP/1.2 200');
+    expect(response.statusLine()).toEqual('HTTP/1.2 200');
   });
 
   it('creates HttpResponse with 404', () => {
     const response = new HttpResponse({ httpVersion: 1.1, status: 404 });
-    expect(response.toString()).toEqual('HTTP/1.1 404');
+    expect(response.statusLine()).toEqual('HTTP/1.1 404');
   });
 
   it('creates HttpResponse with status phrase Created', () => {
     const response = new HttpResponse({ httpVersion: 1.1, status: 201, reasonPhrase: 'Created' });
-    expect(response.toString()).toEqual('HTTP/1.1 201 Created');
+    expect(response.statusLine()).toEqual('HTTP/1.1 201 Created');
   });
 });
