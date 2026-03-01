c53dbf06a1628e4e7325fe4059884b87edf61787
Kody Fintak <kody.fintak@gmail.com>
2026-03-01 12:42:57 -0500

    feat: add chaining methods to make creation easier

--- typescript/src/tcp/HttpResponse.test.ts
+++ typescript/src/tcp/HttpResponse.test.ts
@@ -10,6 +10,11 @@ describe('HttpResponse', () => {
     expect(response.toString()).toEqual('HTTP/1.1 200\r\ncontent-type: text/plain\r\ndate: Tue, 29 Oct 2024 16:56:32 GMT\r\n\r\n');
   });
 
+  it('creates HttpResponse with chaining', () => {
+    const response = HttpResponse.success().withHeader('content-type', 'text/plain').withHeader('date', 'Tue, 29 Oct 2024 16:56:32 GMT');
+    expect(response.toString()).toEqual('HTTP/1.1 200\r\ncontent-type: text/plain\r\ndate: Tue, 29 Oct 2024 16:56:32 GMT\r\n\r\n');
+  });
+
   it('creates HttpResponse with Body', () => {
     const response = new HttpResponse({
       httpVersion: 1.1,
@@ -23,17 +28,17 @@ describe('HttpResponse', () => {
   });
 
   it('creates HttpResponse with 1.2 version', () => {
-    const response = new HttpResponse({ httpVersion: 1.2, status: 200 });
+    const response = new HttpResponse({ httpVersion: 1.2, status: 200, headers: {} });
     expect(response.statusLine()).toEqual('HTTP/1.2 200');
   });
 
   it('creates HttpResponse with 404', () => {
-    const response = new HttpResponse({ httpVersion: 1.1, status: 404 });
+    const response = new HttpResponse({ httpVersion: 1.1, status: 404, headers: {} });
     expect(response.statusLine()).toEqual('HTTP/1.1 404');
   });
 
   it('creates HttpResponse with status phrase Created', () => {
-    const response = new HttpResponse({ httpVersion: 1.1, status: 201, reasonPhrase: 'Created' });
+    const response = new HttpResponse({ httpVersion: 1.1, status: 201, reasonPhrase: 'Created', headers: {} });
     expect(response.statusLine()).toEqual('HTTP/1.1 201 Created');
   });
 });
--- typescript/src/tcp/HttpResponse.ts
+++ typescript/src/tcp/HttpResponse.ts
@@ -1,7 +1,15 @@
 export class HttpResponse {
   constructor(
-    private message: { httpVersion: number; status: number; reasonPhrase?: string; headers?: Record<string, string>; body?: string }
+    private message: { httpVersion: number; status: number; reasonPhrase?: string; headers: Record<string, string>; body?: string }
   ) {}
+
+  static success() {
+    return new HttpResponse({ httpVersion: 1.1, status: 200, headers: {} });
+  }
+
+  withHeader(key: string, value: string) {
+    return new HttpResponse({ ...this.message, headers: { ...this.message.headers, [key]: value } });
+  }
 
   toString() {
     const statusLineAndHeaderLines = `${[this.statusLine(), ...this.headerLines()].join('\r\n')}\r\n\r\n`;
