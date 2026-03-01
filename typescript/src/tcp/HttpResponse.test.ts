85cd77df25e1930af379b4579c9021a4d0c7bff1
Kody Fintak <kody.fintak@gmail.com>
2026-03-01 12:31:50 -0500

    feat: add body to the toString

--- typescript/src/tcp/HttpResponse.test.ts
+++ typescript/src/tcp/HttpResponse.test.ts
@@ -1,8 +1,12 @@
 class HttpResponse {
-  constructor(private message: { httpVersion: number; status: number; reasonPhrase?: string; headers?: Record<string, string> }) {}
+  constructor(
+    private message: { httpVersion: number; status: number; reasonPhrase?: string; headers?: Record<string, string>; body?: string }
+  ) {}
 
   toString() {
-    return `${[this.statusLine(), ...this.headerLines()].join('\r\n')}\r\n\r\n`;
+    const statusLineAndHeaderLines = `${[this.statusLine(), ...this.headerLines()].join('\r\n')}\r\n\r\n`;
+    if (this.message.body) return `${statusLineAndHeaderLines}${this.message.body}`;
+    return statusLineAndHeaderLines;
   }
 
   private headerLines() {
@@ -25,6 +29,18 @@ describe('HttpResponse', () => {
       headers: { 'content-type': 'text/plain', date: 'Tue, 29 Oct 2024 16:56:32 GMT' }
     });
     expect(response.toString()).toEqual('HTTP/1.1 200\r\ncontent-type: text/plain\r\ndate: Tue, 29 Oct 2024 16:56:32 GMT\r\n\r\n');
+  });
+
+  it('creates HttpResponse with Body', () => {
+    const response = new HttpResponse({
+      httpVersion: 1.1,
+      status: 200,
+      headers: { 'content-type': 'text/plain', date: 'Tue, 29 Oct 2024 16:56:32 GMT' },
+      body: 'Hello World'
+    });
+    expect(response.toString()).toEqual(
+      'HTTP/1.1 200\r\ncontent-type: text/plain\r\ndate: Tue, 29 Oct 2024 16:56:32 GMT\r\n\r\nHello World'
+    );
   });
 
   it('creates HttpResponse with 1.2 version', () => {
