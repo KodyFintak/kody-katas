426ceb3548eedeb51161a195f86eaefa9bb7454d
Kody Fintak <kody.fintak@gmail.com>
2026-03-01 12:03:56 -0500

    feat: status on response

--- typescript/src/tcp/HttpResponse.test.ts
+++ typescript/src/tcp/HttpResponse.test.ts
@@ -1,19 +1,24 @@
 class HttpResponse {
-  constructor(private message: { httpVersion: number }) {}
+  constructor(private message: { httpVersion: number; status: number }) {}
 
   toString() {
-    return `HTTP/${this.message.httpVersion}`;
+    return `HTTP/${this.message.httpVersion} ${this.message.status}`;
   }
 }
 
 describe('HttpResponse', () => {
   it('creates HttpResponse', () => {
-    const response = new HttpResponse({ httpVersion: 1.1 });
-    expect(response.toString()).toEqual('HTTP/1.1');
+    const response = new HttpResponse({ httpVersion: 1.1, status: 200 });
+    expect(response.toString()).toEqual('HTTP/1.1 200');
   });
 
   it('creates HttpResponse with 1.2 version', () => {
-    const response = new HttpResponse({ httpVersion: 1.2 });
-    expect(response.toString()).toEqual('HTTP/1.2');
+    const response = new HttpResponse({ httpVersion: 1.2, status: 200 });
+    expect(response.toString()).toEqual('HTTP/1.2 200');
+  });
+
+  it('creates HttpResponse with 404', () => {
+    const response = new HttpResponse({ httpVersion: 1.1, status: 404 });
+    expect(response.toString()).toEqual('HTTP/1.1 404');
   });
 });
