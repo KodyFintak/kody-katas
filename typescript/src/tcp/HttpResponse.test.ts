d1e83b24b477250c8b48cdb9c4998ffcb087950b
Kody Fintak <kody.fintak@gmail.com>
2026-03-01 12:06:53 -0500

    feat: add optional reason phrase

--- typescript/src/tcp/HttpResponse.test.ts
+++ typescript/src/tcp/HttpResponse.test.ts
@@ -1,8 +1,14 @@
 class HttpResponse {
-  constructor(private message: { httpVersion: number; status: number }) {}
+  constructor(private message: { httpVersion: number; status: number; reasonPhrase?: string }) {}
 
   toString() {
-    return `HTTP/${this.message.httpVersion} ${this.message.status}`;
+    return this.statusLine();
+  }
+
+  private statusLine() {
+    const statusLine = `HTTP/${this.message.httpVersion} ${this.message.status}`;
+    if (this.message.reasonPhrase) return `${statusLine} ${this.message.reasonPhrase}`;
+    return statusLine;
   }
 }
 
@@ -20,5 +26,10 @@ describe('HttpResponse', () => {
   it('creates HttpResponse with 404', () => {
     const response = new HttpResponse({ httpVersion: 1.1, status: 404 });
     expect(response.toString()).toEqual('HTTP/1.1 404');
+  });
+
+  it('creates HttpResponse with status phrase Created', () => {
+    const response = new HttpResponse({ httpVersion: 1.1, status: 201, reasonPhrase: 'Created' });
+    expect(response.toString()).toEqual('HTTP/1.1 201 Created');
   });
 });
