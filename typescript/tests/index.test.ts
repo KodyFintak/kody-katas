f037017dcf96fdd4e8ccf88caaa682448f83546f
Kody Fintak <kody.fintak@gmail.com>
2026-04-12 20:25:56 -0400

    feat: separate npm scripts for node and tcp

--- typescript/tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { startServer } from '../src/index';
-
-describe('test', () => {
-  it('starts a http server', () => {
-    const server = startServer();
-
-    expect(server).toEqual(2);
-  });
-});
