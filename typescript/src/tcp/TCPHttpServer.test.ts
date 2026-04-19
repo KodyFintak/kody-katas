70ff53fb067c7c8a148689dfaf45e0b115962dd7
Kody Fintak <kody.fintak@gmail.com>
2026-04-19 17:52:05 -0400

    clean: noop logger

--- typescript/src/tcp/TCPHttpServer.test.ts
+++ typescript/src/tcp/TCPHttpServer.test.ts
@@ -1,20 +1,25 @@
-import { TCPHttpServer } from './TCPHttpServer';
+import { Logger, TCPHttpServer } from './TCPHttpServer';
 import { createRequest } from './http-request-fixture';
 import { HttpResponse } from './HttpResponse';
 import { handleRequest } from './handle-request';
 import { Socket } from 'node:net';
 
+const noopLogger: Logger = {
+  log: (message: any): void => {},
+  error: (message: any): void => {}
+};
+
 describe('TCPHttpServer', () => {
   it('handles GET request', () => {
     const request = createRequest({ method: 'GET' });
-    const server = new TCPHttpServer({ onRequest: handleRequest });
+    const server = new TCPHttpServer({ onRequest: handleRequest, logger: noopLogger });
     const response = server.handleRequest(request);
     expect(response).toEqual(HttpResponse.success().withJsonBody({ name: 'kody' }));
   });
 
   it('handles socket write', () => {
     const messageAsString = 'POST / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: keep-alive\r\n\r\n';
-    const server = new TCPHttpServer({ onRequest: handleRequest });
+    const server = new TCPHttpServer({ onRequest: handleRequest, logger: noopLogger });
     const socket = new SpySocket();
     server.onData(socket as unknown as Socket, messageAsString);
     expect(socket.isAlive).toEqual(false);
