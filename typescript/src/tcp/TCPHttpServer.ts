f721e6daba86bf402a7365a1bfe3849f657f9b13
Kody Fintak <kody.fintak@gmail.com>
2026-04-19 17:50:25 -0400

    clean: logger class property

--- typescript/src/tcp/TCPHttpServer.ts
+++ typescript/src/tcp/TCPHttpServer.ts
@@ -3,25 +3,32 @@ import { HttpRequest } from './HttpRequest';
 import { handleRequest } from './handle-request';
 import { HttpResponse } from './HttpResponse';
 
+export interface Logger {
+  log: (message: any) => void;
+  error: (message: any) => void;
+}
+
 export class TCPHttpServer {
   private readonly port: number;
   private readonly onRequest: (request: HttpRequest) => HttpResponse;
+  private readonly logger: Logger;
 
-  constructor(options: { port?: number; onRequest?: (request: HttpRequest) => HttpResponse } = {}) {
+  constructor(options: { port?: number; onRequest?: (request: HttpRequest) => HttpResponse; logger?: Logger } = {}) {
     this.port = options.port ?? 3000;
     this.onRequest = options.onRequest ?? handleRequest;
+    this.logger = options.logger ?? console;
   }
 
   start() {
     const server = net.createServer(socket => {
       socket.setEncoding('utf8');
       socket.on('data', data => this.onData(socket, data));
-      socket.on('end', () => console.log('client disconnected'));
-      socket.on('error', console.error);
+      socket.on('end', () => this.logger.log('client disconnected'));
+      socket.on('error', this.logger.error);
     });
 
     server.listen(this.port);
-    console.log(`started server on port ${this.port}`);
+    this.logger.log(`started server on port ${this.port}`);
   }
 
   handleRequest(request: HttpRequest) {
@@ -31,7 +38,7 @@ export class TCPHttpServer {
   onData(socket: Socket, data: string | Buffer<ArrayBuffer>) {
     const requestAsString = Buffer.from(data).toString();
     const request = HttpRequest.parse(requestAsString);
-    console.log(request);
+    this.logger.log(request);
     const response = this.onRequest(request);
     socket.write(response.toString());
     socket.end();
