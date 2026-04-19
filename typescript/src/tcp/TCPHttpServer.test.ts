33ae53ab638635f4594c82dae04f7062cbd4a048
Kody Fintak <kody.fintak@gmail.com>
2026-04-19 18:04:10 -0400

    clean: use real example and localized handle request

--- typescript/src/tcp/TCPHttpServer.test.ts
+++ typescript/src/tcp/TCPHttpServer.test.ts
@@ -1,35 +1,40 @@
 import { createRequest } from './http-request-fixture';
 import { HttpResponse } from './HttpResponse';
-import { handleRequest } from './handle-request';
 import { Socket } from 'node:net';
 import { createTestServer } from './createTestServer';
+import { HttpRequest } from './HttpRequest';
+
+function handleRequest(request: HttpRequest): HttpResponse {
+  return HttpResponse.success().withTextBody(`Hello ${request.bodyAsObject().name}`);
+}
 
 describe('TCPHttpServer', () => {
   it('handles GET request', () => {
-    const request = createRequest({ method: 'GET' });
+    const request = createRequest({ method: 'GET', rawBody: JSON.stringify({ name: 'Kody' }) });
     const server = createTestServer(handleRequest);
     const response = server.handleRequest(request);
-    expect(response).toEqual(HttpResponse.success().withJsonBody({ name: 'kody' }));
+    expect(response).toEqual(HttpResponse.success().withTextBody('Hello Kody'));
   });
 
   it('handles socket write', () => {
-    const messageAsString = 'POST / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: keep-alive\r\n\r\n';
+    const messageAsString =
+      'GET / HTTP/1.1\r\nHost: localhost:3000\r\ncontent-type: application/json\r\ncontent-length: 18\r\n\r\n{ "name": "Kody" }';
     const server = createTestServer(handleRequest);
     const socket = new SpySocket();
     server.onData(socket as unknown as Socket, messageAsString);
     expect(socket.isAlive).toEqual(false);
-    expect(socket.writtenData).toEqual(HttpResponse.success().withTextBody('Hello ').toString());
+    expect(socket.writtenData).toEqual(HttpResponse.success().withTextBody('Hello Kody').toString());
   });
 
   it('handles incomplete message', () => {
-    const bufferedMessage1 = "POST / HTTP/1.1\r\nHost: localhost:3000'";
-    const bufferedMessage2 = '\r\nConnection: keep-alive\r\n\r\n';
+    const bufferedMessage1 = 'GET / HTTP/1.1\r\nHost: localhost:3000\r\ncontent-type: application/json';
+    const bufferedMessage2 = '\r\ncontent-length: 18\r\n\r\n{ "name": "Kody" }';
     const server = createTestServer(handleRequest);
     const socket = new SpySocket();
     server.onData(socket as unknown as Socket, bufferedMessage1);
     server.onData(socket as unknown as Socket, bufferedMessage2);
     expect(socket.isAlive).toEqual(false);
-    expect(socket.writtenData).toEqual(HttpResponse.success().withTextBody('Hello ').toString());
+    expect(socket.writtenData).toEqual(HttpResponse.success().withTextBody('Hello Kody').toString());
   });
 });
 
